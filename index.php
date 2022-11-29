b82f218886ab43c2378219f55b1542bc57fb9f83
giadavolpin <volpingiada@gmail.com>
2022-11-29 19:03:55 +0100

    stringhe varie

--- index.php
+++ index.php
@@ -13,6 +13,14 @@ Milestone 4 (BONUS)
 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
 Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali -->
 
+<?php 
+/* var_dump($_GET ['characters']); */
+$alfabeto = 'abcdefghilmnopqrstuvwxyz';
+$numero = '0123456789';
+$simboli = '!?&%$<>/()[]{}@#';
+$password = '';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,15 +28,54 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>password</title>
 </head>
 
 <body>
+    <h1>Genera una password</h1>
     <form action="index.php" method="GET">
-        <label for="text">Generatore password</label>
+        <!-- il form ti da il punsante per hambuger menu -->
+        <label for="text">Lunghezza password:</label>
         <input type="number" id="count" name="numero"></input>
         <button type="submit">invia</button>
+        <button type="submit">cancella</button>
     </form>
+    <!--  <div class="col-sm-5">
+        <div class="form-check">
+            <input type="radio" checked value="1">
+            <label for="allow-duplicates" class="form-check-label">Si</label>
+        </div>
+        <div class="form-check">
+            <input type="radio" checked value="0">
+            <label for="allow-duplicates" class="form-check-label">No</label>
+        </div>
+    </div>
+ -->
+    <div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="S" id="characters" name="characters[]">
+            <label class="form-check-label" for="characters">
+                Simboli
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="N" id="characters" name="characters[]">
+            <label class="form-check-label" for="characters">
+                Numeri
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="L" id="characters" name="characters[]">
+            <label class="form-check-label" for="characters">
+                Lettere
+            </label>
+        </div>
+    </div>
+
+
+
     <?php 
     if (isset($_GET['numero']) && $_GET['numero'] === '')
 
