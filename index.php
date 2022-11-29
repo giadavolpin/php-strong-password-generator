69083ae270dd8bb0a87e1d69405921a0eb244976
giadavolpin <volpingiada@gmail.com>
2022-11-29 17:57:42 +0100

    form

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ Descrizione
 Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
 L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
 Milestone 1
-Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Creare un form che invii in GET la lunghezza(count) della password. Una nostra funzione utilizzerà questo dato per generare una password casuale [RANDOM] (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
 Scriviamo tutto (logica e layout) in un unico file index.php
 Milestone 2
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
@@ -13,13 +13,30 @@ Milestone 4 (BONUS)
 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
 Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali -->
 
-GET count
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-<form action="index.php" method="GET">
-    <input type="text">
+<body>
+    <form action="index.php" method="GET">
+        <label for="text">Generatore password</label>
+        <input type="number" id="count" name="numero"></input>
+        <button type="submit">invia</button>
+    </form>
+    <?php 
+    if (isset($_GET['numero']) && $_GET['numero'] === '')
 
-</form>
+    ?>
+
+</body>
+
+</html>
 
 
 
